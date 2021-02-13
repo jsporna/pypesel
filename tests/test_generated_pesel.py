db9d8818313268911e0cdc1618989ebc9f173dfd
Jakub Spórna <jakub.sporna@gmail.com>
2021-02-13 09:54:10 +0100

    tests reimplemented to pytest and add more randomness

--- tests/test_generated_pesel.py
+++ tests/test_generated_pesel.py
@@ -1,25 +1,34 @@
 from pesel import Pesel
-import unittest
+import pytest
 
 
-class GeneratedPeselTestCase(unittest.TestCase):
-    def setUp(self) -> None:
-        self.pesel = Pesel.generate()
+@pytest.fixture(scope='session', params=[Pesel.generate() for _ in range(100)])
+def pesel_obj(request):
+    return request.param
 
-    def test_generated_pesel(self):
-        self.assertIsInstance(self.pesel.value, str)
 
-    def test_generated_pesel_gender(self):
-        self.assertIn(self.pesel.gender, ('male', 'female'))
+def test_generated_pesel(pesel_obj):
+    with pytest.assume:
+        assert isinstance(pesel_obj.value, str)
+        assert len(pesel_obj.value) == 11
+        assert pesel_obj.value.isdigit()
 
-    def test_generated_pesel_male(self):
-        self.assertIsInstance(self.pesel.male, bool)
 
-    def test_generated_pesel_year(self):
-        self.assertTrue(Pesel.YEAR_MIN <= self.pesel.year <= Pesel.YEAR_MAX)
+def test_generated_pesel_gender(pesel_obj):
+    pytest.assume(pesel_obj.gender in ('male', 'female'))
 
-    def test_generated_pesel_month(self):
-        self.assertTrue(1 <= self.pesel.month <= 12)
 
-    def test_generated_pesel_day(self):
-        self.assertTrue(1 <= self.pesel.day <= 31)
+def test_generated_pesel_male(pesel_obj):
+    pytest.assume(isinstance(pesel_obj.male, bool))
+
+
+def test_generated_pesel_year(pesel_obj):
+    pytest.assume(Pesel.YEAR_MIN <= pesel_obj.year <= Pesel.YEAR_MAX)
+
+
+def test_generated_pesel_month(pesel_obj):
+    pytest.assume(1 <= pesel_obj.month <= 12)
+
+
+def test_generated_pesel_day(pesel_obj):
+    pytest.assume(1 <= pesel_obj.day <= 31)
--- tests/test_incorrect_pesel.py
+++ tests/test_incorrect_pesel.py
@@ -1,11 +1,47 @@
 from pesel import Pesel
-import unittest
+import random
+from datetime import datetime
+import pytest
+
+random.seed(datetime.now())
 
 
-class CorrectPeselTestCase(unittest.TestCase):
-    def setUp(self) -> None:
-        self.value = "65432101238"
+@pytest.fixture(scope="session")
+def incorrect_pesel_value():
+    return "65432101238"
 
-    def test_incorrect_pesel(self):
-        with self.assertRaises(ValueError):
-            p = Pesel(self.value)
+
+def test_incorrect_pesel(incorrect_pesel_value):
+    with pytest.raises(ValueError):
+        Pesel(incorrect_pesel_value)
+
+
+@pytest.mark.parametrize('year', [random.randint(0, 1799), random.randint(2300, 10000)])
+def test_incorrect_year(year):
+    with pytest.raises(ValueError):
+        Pesel.generate(year=year)
+
+
+@pytest.mark.parametrize('month', [0, random.randint(13, 100)])
+def test_incorrect_month(month):
+    with pytest.raises(ValueError):
+        Pesel.generate(month=month)
+
+
+@pytest.mark.parametrize('day', [0, random.randint(32, 100)])
+def test_incorrect_day(day):
+    with pytest.raises(ValueError):
+        Pesel.generate(day=day)
+
+
+@pytest.mark.parametrize(('month', 'day'), [(1, 32), (2, 30), (3, 32), (4, 31), (5, 32), (6, 31),
+                                            (7, 32), (8, 32), (9, 31), (10, 32), (11, 31), (12, 32)])
+def test_incorrect_month_day(month, day):
+    with pytest.raises(ValueError):
+        Pesel.generate(month=month, day=day)
+
+
+@pytest.mark.parametrize(('year', 'month', 'day'), [(1832, 2, 30), (1831, 2, 29), (1900, 2, 29)])
+def test_incorrect_year_month_day(year, month, day):
+    with pytest.raises(ValueError):
+        Pesel.generate(year=year, month=month, day=day)
